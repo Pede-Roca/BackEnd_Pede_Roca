cbdbb9b0c906b2db1d8ccc6b0f2cde09e02986e9
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-28 19:36:40 -0300

    #Update - CategoriaRepository

---
+++ Pede_RocaAPP/Pede_RocaAPP.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pede_RocaAPP.Domain.Entities;
+
+namespace Pede_RocaAPP.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
