75484c821b6db76be06cf8da7618db23f96ec22a
phs-santos <paulo18santos2000@gmail.com>
2024-09-29 20:12:58 -0300

    #update - adicionado os services de usuario, interface de service de usuario e correcao em interface de repositorio de usuario

--- Pede_RocaAPP/Pede_RocaAPP.Domain/Interfaces/IUsuarioRepository.cs
+++ Pede_RocaAPP/Pede_RocaAPP.Domain/Interfaces/IUsuarioRepository.cs
@@ -4,7 +4,7 @@ namespace Pede_RocaAPP.Domain.Interfaces
 {
     public interface IUsuarioRepository
     {
-        Task<Usuario> AdicionarAsync(Endereco usuario);
+        Task<Usuario> AdicionarAsync(Usuario usuario);
         Task<Usuario> AtualizarAsync(Guid id, Usuario usuario);
         Task<Usuario> DeleteAsync(Usuario usuario);
         Task<Usuario> GetByIdAsync(Guid id);
