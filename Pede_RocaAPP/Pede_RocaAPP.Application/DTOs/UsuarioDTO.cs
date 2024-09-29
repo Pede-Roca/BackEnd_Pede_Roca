48fc6c41f1ae398a537ba1d0013eeaf6350536b5
phs-santos <paulo18santos2000@gmail.com>
2024-09-29 17:12:43 -0300

    #update - UsuarioDTO

---
+++ Pede_RocaAPP/Pede_RocaAPP.Application/DTOs/UsuarioDTO.cs
@@ -0,0 +1,57 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pede_RocaAPP.Application.DTOs
+{
+    public class UsuarioDTO
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail deve ser um endereço de e-mail válido.")]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
+        [DisplayName("Senha")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "O Nome é obrigatório.")]
+        [MinLength(3, ErrorMessage = "O Nome deve ter no mínimo 3 caracteres.")]
+        [MaxLength(100, ErrorMessage = "O Nome deve ter no máximo 100 caracteres.")]
+        [DisplayName("Nome")]
+        public string Nome { get; set; }
+
+        [Phone(ErrorMessage = "O telefone deve ser um número de telefone válido.")]
+        [DisplayName("Telefone")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "O CPF deve ter 11 dígitos.")]
+        [DisplayName("CPF")]
+        public string Cpf { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Data de Nascimento")]
+        public DateTime DataNasc { get; set; }
+
+        [Required(ErrorMessage = "O status é obrigatório.")]
+        [DisplayName("Status")]
+        public bool Status { get; set; }
+
+        [Required(ErrorMessage = "O nível de acesso é obrigatório.")]
+        [DisplayName("Nível de Acesso")]
+        public string NivelAcesso { get; set; }
+
+        [DisplayName("Foto de Perfil")]
+        public string UidFotoPerfil { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Data de Criação da Conta")]
+        public DateTime CreateUserDate { get; set; }
+
+        public Guid IdEndereco { get; set; }
+    }
+}
