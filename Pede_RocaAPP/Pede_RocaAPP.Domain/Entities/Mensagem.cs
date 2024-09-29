b7ca51320653dc8ae2f20870d5a6ab7398dddf0c
phs-santos <paulo18santos2000@gmail.com>
2024-09-29 20:38:21 -0300

    #Update - Adicionado Classe Mensagem

---
+++ Pede_RocaAPP/Pede_RocaAPP.Domain/Entities/Mensagem.cs
@@ -0,0 +1,28 @@
+namespace Pede_RocaAPP.Domain.Entities
+{
+    public class Mensagem
+    {
+        public Guid Id { get; set; }
+        public Usuario IdUsuario { get; set; }
+        public DateTime Data { get; set; }
+        public string Email { get; set; }
+        public string Conteudo { get; set; }
+        public string UidAnexo { get; set; }
+        public string Status { get; set; }
+
+        public Mensagem()
+        {
+        }
+
+        public Mensagem(Usuario idUsuario, DateTime data, string email, string conteudo, string uidAnexo, string status)
+        {
+            Id = Guid.NewGuid();
+            IdUsuario = idUsuario;
+            Data = data;
+            Email = email;
+            Conteudo = conteudo;
+            UidAnexo = uidAnexo;
+            Status = status;
+        }
+    }
+}
