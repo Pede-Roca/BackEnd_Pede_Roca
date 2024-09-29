0c31e7628b2718a38e34c60b0875a8b3045d1ee5
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-29 17:03:07 -0300

    #update UnidadeMedidaRepository

--- Pede_RocaAPP/Pede_RocaAPP.Infra.Data/Context/ApplicationDbContext.cs
+++ Pede_RocaAPP/Pede_RocaAPP.Infra.Data/Context/ApplicationDbContext.cs
@@ -9,6 +9,7 @@ namespace Pede_RocaAPP.Infra.Data.Context
         
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<UnidadeMedida> unidadeMedidas { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
---
+++ Pede_RocaAPP/Pede_RocaAPP.Infra.Data/Repositories/UnidadeMedidaRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pede_RocaAPP.Domain.Entities;
+using Pede_RocaAPP.Domain.Interfaces;
+using Pede_RocaAPP.Infra.Data.Context;
+
+namespace Pede_RocaAPP.Infra.Data.Repositories
+{
+    public class UnidadeMedidaRepository : IUnidadeMedidaRepository
+    {
+        private ApplicationDbContext _unidadeMedidaContext;
+
+        public UnidadeMedidaRepository(ApplicationDbContext context)
+        {
+            _unidadeMedidaContext = context;
+        }
+
+        public async Task<UnidadeMedida> AdicionarAsync(UnidadeMedida unidadeMedida)
+        {
+            _unidadeMedidaContext.Add(unidadeMedida);
+            await _unidadeMedidaContext.SaveChangesAsync();
+            return unidadeMedida;
+        }
+
+        public async Task<UnidadeMedida> AtualizarAsync(Guid id, UnidadeMedida unidadeMedida)
+        {
+            _unidadeMedidaContext.Update(unidadeMedida);
+            await _unidadeMedidaContext.SaveChangesAsync();
+            return unidadeMedida;
+        }
+
+        public async Task<UnidadeMedida> DeleteAsync(UnidadeMedida unidadeMedida)
+        {
+            _unidadeMedidaContext.Remove(unidadeMedida);
+            await _unidadeMedidaContext.SaveChangesAsync();
+            return unidadeMedida;
+        }
+
+        public async Task<UnidadeMedida> GetByIdAsync(Guid id)
+        {
+            var unidadeMedida = await _unidadeMedidaContext.unidadeMedidas.FindAsync(id);
+            return unidadeMedida;
+
+        }
+
+        public async Task<IEnumerable<UnidadeMedida>> GetAllAsync()
+        {
+            return await _unidadeMedidaContext.unidadeMedidas.OrderBy(c => c.NomeUnidade).ToListAsync();
+        }
+    }
+}
