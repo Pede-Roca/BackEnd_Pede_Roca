d6129a0c0a1df14c3da08e94218ecc82198a9a13
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-29 16:32:10 -0300

    #Updade UnidadeMedidaService

---
+++ Pede_RocaAPP/Pede_RocaAPP.Application/Services/UnidadeMedidaService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Pede_RocaAPP.Application.DTOs;
+using Pede_RocaAPP.Domain.Entities;
+using Pede_RocaAPP.Domain.Interfaces;
+
+namespace Pede_RocaAPP.Application.Services
+{
+    public class UnidadeMedidaService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnidadeMedidaRepository _unidadeMedidaRepository;
+
+        public UnidadeMedidaService(IUnidadeMedidaRepository unidadeMedidaRepository, IMapper mapper)
+        {
+            _unidadeMedidaRepository = unidadeMedidaRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AdicionarAsync(UnidadeMedidaDTO unidadeMedidaDTO)
+        {
+            var unidadeMedidaEntity = _mapper.Map<UnidadeMedida>(unidadeMedidaDTO);
+            await _unidadeMedidaRepository.AdicionarAsync(unidadeMedidaEntity);
+        }
+
+        public async Task AtualziarAsync(Guid id, UnidadeMedidaDTO unidadeMedidaDTO)
+        {
+            var unidadeMedidaEntity = _mapper.Map<UnidadeMedida>(unidadeMedidaDTO);
+            await _unidadeMedidaRepository.AtualizarAsync(id, unidadeMedidaEntity);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var unidadeMedidaEntity = await _unidadeMedidaRepository.GetByIsAsync(id);
+            await _unidadeMedidaRepository.DeleteAsync(unidadeMedidaEntity);
+        }
+
+        public async Task<UnidadeMedidaDTO> getByIdAsync(Guid id)
+        {
+            var unidadeMedidaEntity = await _unidadeMedidaRepository.GetByIsAsync(id);
+            return _mapper.Map<UnidadeMedidaDTO>(unidadeMedidaEntity);
+        }
+
+        public async Task<IEnumerable<UnidadeMedidaDTO>> GetAllAsync()
+        {
+            var unidadeMedidaEntity = await _unidadeMedidaRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<UnidadeMedidaDTO>>(unidadeMedidaEntity);
+        }
+    }
+}
