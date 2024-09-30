383fa3407c7a45489177ee9070b4a2771d3c4714
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-30 14:34:56 -0300

    #update DTO de PlanoAssinatura

---
+++ Pede_RocaAPP/Pede_RocaAPP.Application/DTOs/PlanoAssinaturaDTO.cs
@@ -0,0 +1,25 @@
+﻿using Pede_RocaAPP.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pede_RocaAPP.Application.DTOs
+{
+    public class PlanoAssinaturaDTO
+    {
+        public Guid id {  get; set; }
+
+        [Required(ErrorMessage = "O ID do usuário é obrigatório")]
+        [DisplayName("ID do Usuário")]
+        public Usuario idUsuario { get; set; }
+
+        [Required(ErrorMessage = "O preço é obrigatório")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DisplayName("Preço")]
+        public decimal preco { get; set; }
+        [Required(ErrorMessage = "O status do plano é obrigatório")]
+        [DisplayName("Status")]
+        public bool Status { get; set; }
+    }
+}
