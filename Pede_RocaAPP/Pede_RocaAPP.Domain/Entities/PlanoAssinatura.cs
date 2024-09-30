9b75bc0c24ac097ab6237e65ef929ad5e2981f38
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-30 14:15:24 -0300

    #update Criação da Entidade Usuário em Domain

---
+++ Pede_RocaAPP/Pede_RocaAPP.Domain/Entities/PlanoAssinatura.cs
@@ -0,0 +1,22 @@
+﻿namespace Pede_RocaAPP.Domain.Entities
+{
+    public class PlanoAssinatura
+    {
+        public Guid Id { get; set; }
+        public decimal Preco {  get; set; }
+        public bool Ativo {  get; set; }
+        public Usuario IdUsuario { get; set; }
+
+        public PlanoAssinatura()
+        {
+        }
+
+        public PlanoAssinatura(Usuario idUsuario, decimal preco, bool ativo)
+        {
+            Id = Guid.NewGuid();
+            IdUsuario = idUsuario;
+            Preco = preco;
+            Ativo = ativo;
+        }
+    }
+}
