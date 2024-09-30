c456bc1c4af60ebc72f59681aa36afcb67d0c281
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-30 15:12:22 -0300

    #update IPlanoAssinaturaService

---
+++ Pede_RocaAPP/Pede_RocaAPP.Application/Interface/IPlanoAssinaturaService.cs
@@ -0,0 +1,13 @@
+﻿using Pede_RocaAPP.Application.DTOs;
+
+namespace Pede_RocaAPP.Application.Interface
+{
+    public interface IPlanoAssinaturaService
+    {
+        Task AdicionarAsync(PlanoAssinaturaDTO planoAssinaturaDTO);
+        Task AtualizarAsync(Guid id, PlanoAssinaturaDTO planoAssinaturaDTO);
+        Task DeleteAsync(Guid id);
+        Task<PlanoAssinaturaDTO> GetByIdAsync(Guid id);
+        Task<IEnumerable<PlanoAssinaturaDTO>> GetAllAsync();
+    }
+}
