0ca0859f3b42b3bcab02afeffbc7345885931055
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-30 17:55:00 -0300

    #update Implementação do ProdutoRepository

---
+++ Pede_RocaAPP/Pede_RocaAPP.Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pede_RocaAPP.Domain.Entities;
+using Pede_RocaAPP.Infra.Data.Context;
+
+namespace Pede_RocaAPP.Infra.Data.Repositories
+{
+    public class ProdutoRepository
+    {
+        private ApplicationDbContext _produtoContext;
+
+        public ProdutoRepository(ApplicationDbContext produtoContext)
+        {
+            _produtoContext = produtoContext;
+        }
+
+        public async Task<Produto> AdicionarAsync(Produto produto)
+        {
+            _produtoContext.Add(produto);
+            await _produtoContext.SaveChangesAsync();
+            return produto;
+        }
+
+        public async Task<Produto> AtualizarAsync(Guid id, Produto produto)
+        {
+            _produtoContext.Update(produto);
+            await _produtoContext.SaveChangesAsync();
+            return produto;
+        }
+
+        public async Task<Produto> DeleteAsync(Produto produto)
+        {
+            _produtoContext.Remove(produto);
+            await _produtoContext.SaveChangesAsync();
+            return produto;
+        }
+
+        public async Task<Produto> GetByIdAsync(Guid id)
+        {
+            var produto = await _produtoContext.Produtos.FindAsync(id);
+            return produto;
+        }
+
+        public async Task<IEnumerable<Produto>> GetAllAsync()
+        {
+            return await _produtoContext.Produtos.OrderBy(p => p.Nome).ToListAsync();
+        }
+    }
+}
