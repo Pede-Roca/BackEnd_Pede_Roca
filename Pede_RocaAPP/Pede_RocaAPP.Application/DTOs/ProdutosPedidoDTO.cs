900af28ce4a45bbe917408fffacd950dfe46d508
phs-santos <paulo18santos2000@gmail.com>
2024-09-30 20:54:47 -0300

    #update - Adicionando o DTO de ProdutosPedido

---
+++ Pede_RocaAPP/Pede_RocaAPP.Application/DTOs/ProdutosPedidoDTO.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Pede_RocaAPP.Domain.Entities;
+
+namespace Pede_RocaAPP.Application.DTOs
+{
+    public class ProdutosPedidoDTO
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "A quantidade do produto é obrigatória.")]
+        [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser no mínimo 1.")]
+        [DisplayName("Quantidade do Produto")]
+        public int QuantidadeProduto { get; set; }
+
+        [Required(ErrorMessage = "O ID do produto é obrigatório.")]
+        [DisplayName("ID do Produto")]
+        public Produto IdProduto { get; set; }
+    }
+}
