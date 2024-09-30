c2a376bb30967171015d7a196c58c3d66e2da0f1
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-30 17:45:27 -0300

    #update adicionado IProdutoService e ProdutoService ao projeto

---
+++ Pede_RocaAPP/Pede_RocaAPP.Application/Interface/IProdutoService.cs
@@ -0,0 +1,18 @@
+﻿using Pede_RocaAPP.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pede_RocaAPP.Application.Interface
+{
+    public interface IProdutoService
+    {
+        Task AdicionarAsync(ProdutoDTO produtoDTO);
+        Task AtualizarAsync(Guid id, ProdutoDTO produtoDTO);
+        Task DeleteAsync(Guid id);
+        Task<ProdutoDTO> GetByIdAsync(Guid id);
+        Task<IEnumerable<ProdutoDTO>> GetAllAsync();
+    }
+}
