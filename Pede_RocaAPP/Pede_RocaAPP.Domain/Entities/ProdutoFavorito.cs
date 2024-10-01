50af0ba103704f2bdb551a69b7b82fc751d02e8b
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-30 21:36:35 -0300

    #update Adicionado entidade ProdutoFavorito

---
+++ Pede_RocaAPP/Pede_RocaAPP.Domain/Entities/ProdutoFavorito.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pede_RocaAPP.Domain.Entities
+{
+    public class ProdutoFavorito
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Produto IdProduto { get; set; }
+        public Usuario IdUsuario { get; set; }
+
+        public ProdutoFavorito()
+        {
+        }
+
+        public ProdutoFavorito(Guid id, Produto idProduto, Usuario idUsuario)
+        {
+            Id = Guid.NewGuid();
+            IdProduto = idProduto;
+            IdUsuario = idUsuario;
+        }
+    }
+}
