771f27473b5441b7f9da68fe4c5ab4104b1e95bf
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-30 21:41:39 -0300

    #update adicionado IProdutoFavoritoRepository

---
+++ Pede_RocaAPP/Pede_RocaAPP.Domain/Interfaces/IProdutoFavoritoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Pede_RocaAPP.Domain.Entities;
+
+namespace Pede_RocaAPP.Domain.Interfaces
+{
+    public interface IProdutoFavoritoRepository
+    {
+        Task<ProdutoFavorito> AdicionarAsync(ProdutoFavorito produtoFavorito);
+        Task<ProdutoFavorito> AtualizarAsync(Guid id, ProdutoFavorito produtoFavorito);
+        Task<ProdutoFavorito> DeleteAsync(ProdutoFavorito produtoFavorito);
+        Task<ProdutoFavorito> GetByIdAsync(Guid id);
+        Task<IEnumerable<ProdutoFavorito>> GetAllAsync();
+    }
+}
