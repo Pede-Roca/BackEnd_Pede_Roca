a83f9d4cd52bce7e2e0e66fc7c4e1a3a5b78cd50
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-09-30 22:02:55 -0300

    #update adicionado Interface de ProdutoFavoritoService e o serviço ProdutoFavoritoService

---
+++ Pede_RocaAPP/Pede_RocaAPP.Application/Services/ProdutoFavoritoService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Pede_RocaAPP.Application.DTOs;
+using Pede_RocaAPP.Domain.Entities;
+using Pede_RocaAPP.Domain.Interfaces;
+
+namespace Pede_RocaAPP.Application.Services
+{
+    public class ProdutoFavoritoService
+    {
+        private readonly IProdutoFavoritoRepository _produtoFavoritoRepository;
+
+        private readonly IMapper _mapper;
+
+        public ProdutoFavoritoService(IProdutoFavoritoRepository produtoFavoritoRepository, IMapper mapper)
+        {
+            _produtoFavoritoRepository = produtoFavoritoRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AdicionarAsync(ProdutoFavoritoDTO produtoFavoritoDTO)
+        {
+            var produtoFavorito = _mapper.Map<ProdutoFavorito>(produtoFavoritoDTO);
+            await _produtoFavoritoRepository.AdicionarAsync(produtoFavorito);
+        }
+
+        public async Task AtualizarAsync(Guid id, ProdutoFavoritoDTO produtoFavoritoDto)
+        {
+            var produtoFavorito = _mapper.Map<ProdutoFavorito>(produtoFavoritoDto);
+            await _produtoFavoritoRepository.AtualizarAsync(id, produtoFavorito);
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var produtoFavorito = await _produtoFavoritoRepository.GetByIdAsync(id);
+            await _produtoFavoritoRepository.DeleteAsync(produtoFavorito);
+        }
+
+        public async Task<ProdutoFavoritoDTO> GetByIdAsync(Guid id)
+        {
+            var produtoFavorito = await _produtoFavoritoRepository.GetByIdAsync(id);
+            return _mapper.Map<ProdutoFavoritoDTO>(produtoFavorito);
+        }
+
+        public async Task<IEnumerable<ProdutoFavoritoDTO>> GetAllAsync()
+        {
+            var produtosFavoritos = await _produtoFavoritoRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<ProdutoFavoritoDTO>>(produtosFavoritos);
+        }
+    }
+}
