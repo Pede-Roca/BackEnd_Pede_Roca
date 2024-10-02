31a4757755855f28ebca5019718777aee10e2e86
HugoFrajacomo <hugo.frajacomo@fatec.sp.gov.br>
2024-10-02 11:33:51 -0300

    #update migrations

---
+++ Pede_RocaAPP/Pede_RocaAPP.Infra.Data/Migrations/20241002142639_InitialMigrationProd.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pede_RocaAPP.Infra.Data.Migrations
+{
+    public partial class InitialMigrationProd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UnidadeMedidas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NomeUnidade = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    SiglaUnidade = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnidadeMedidas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    CPF = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NivelAcesso = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UidFotoPerfil = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    CreateUserDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    FatorPromocao = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    UidFoto = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    IdCategoria = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdUnidade = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_IdCategoria",
+                        column: x => x.IdCategoria,
+                        principalTable: "Categorias",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Produtos_UnidadeMedidas_IdUnidade",
+                        column: x => x.IdUnidade,
+                        principalTable: "UnidadeMedidas",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarrinhoCompras",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IdUsuario = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarrinhoCompras", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarrinhoCompras_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enderecos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CEP = table.Column<string>(type: "nvarchar(9)", maxLength: 9, nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    Logradouro = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Complemento = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    IdUsuario = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enderecos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Enderecos_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mensagems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Conteudo = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    UidAnexo = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    IdUsuario = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mensagems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mensagems_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "planoAssinaturas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false),
+                    IdUsuario = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_planoAssinaturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_planoAssinaturas_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "produtosFavoritos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdProduto = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdUsuario = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_produtosFavoritos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_produtosFavoritos_Produtos_IdProduto",
+                        column: x => x.IdProduto,
+                        principalTable: "Produtos",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_produtosFavoritos_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Avaliacoes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nota = table.Column<int>(type: "int", nullable: false, defaultValue: 1),
+                    Descricao = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    IdUsuario = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdCarrinhoCompra = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Avaliacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Avaliacoes_CarrinhoCompras_IdCarrinhoCompra",
+                        column: x => x.IdCarrinhoCompra,
+                        principalTable: "CarrinhoCompras",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Avaliacoes_Usuarios_IdUsuario",
+                        column: x => x.IdUsuario,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProdutosPedidos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuantidadeProduto = table.Column<int>(type: "int", nullable: false),
+                    IdProduto = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdCarrinhoCompra = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProdutosPedidos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProdutosPedidos_CarrinhoCompras_IdCarrinhoCompra",
+                        column: x => x.IdCarrinhoCompra,
+                        principalTable: "CarrinhoCompras",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ProdutosPedidos_Produtos_IdProduto",
+                        column: x => x.IdProduto,
+                        principalTable: "Produtos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacoes_IdCarrinhoCompra",
+                table: "Avaliacoes",
+                column: "IdCarrinhoCompra");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacoes_IdUsuario",
+                table: "Avaliacoes",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarrinhoCompras_IdUsuario",
+                table: "CarrinhoCompras",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enderecos_IdUsuario",
+                table: "Enderecos",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mensagems_IdUsuario",
+                table: "Mensagems",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_planoAssinaturas_IdUsuario",
+                table: "planoAssinaturas",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_IdCategoria",
+                table: "Produtos",
+                column: "IdCategoria");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_IdUnidade",
+                table: "Produtos",
+                column: "IdUnidade");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_produtosFavoritos_IdProduto",
+                table: "produtosFavoritos",
+                column: "IdProduto");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_produtosFavoritos_IdUsuario",
+                table: "produtosFavoritos",
+                column: "IdUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProdutosPedidos_IdCarrinhoCompra",
+                table: "ProdutosPedidos",
+                column: "IdCarrinhoCompra");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProdutosPedidos_IdProduto",
+                table: "ProdutosPedidos",
+                column: "IdProduto");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Avaliacoes");
+
+            migrationBuilder.DropTable(
+                name: "Enderecos");
+
+            migrationBuilder.DropTable(
+                name: "Mensagems");
+
+            migrationBuilder.DropTable(
+                name: "planoAssinaturas");
+
+            migrationBuilder.DropTable(
+                name: "produtosFavoritos");
+
+            migrationBuilder.DropTable(
+                name: "ProdutosPedidos");
+
+            migrationBuilder.DropTable(
+                name: "CarrinhoCompras");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "UnidadeMedidas");
+        }
+    }
+}
