318c59ed83c2ba3482e69ee1645b34b4c96a2bad
analivalmeida <analivialpalmeida@gmail.com>
2024-10-10 19:17:20 -0300

    Testes Endereco

---
+++ Pede_RocaAPP/Pede_RocaAPP.Domain.Test/EnderecoUnitTest.cs
@@ -0,0 +1,107 @@
+﻿using FluentAssertions;
+using Pede_RocaAPP.Domain.Entities;
+using Pede_RocaAPP.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pede_RocaAPP.Domain.Test
+{
+    public class EnderecoUnitTest
+    {
+        [Fact(DisplayName = "Criar usuario com dados válidos")]
+        public void CreateEndereco_ParametrosValidos()
+        {
+            Action action = () => new Endereco("15991-510", "Matão", "SP", "Rua Para", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "CEP inválido - Formato errado")]
+        public void CreateEndereco_ParametrosInvalidos_CEP()
+        {
+            Action action = () => new Endereco("15991510", "Matão", "SP", "Rua Para", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("CEP inválido, deve estar no formato 00000-000.");
+        }
+
+        [Fact(DisplayName = "CEP inválido - CEP vazio")]
+        public void CreateEndereco_ParametrosInvalidos_CEPVazio()
+        {
+            Action action = () => new Endereco("", "Matão", "SP", "Rua Para", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("CEP inválido, deve estar no formato 00000-000.");
+        }
+
+        [Fact(DisplayName = "Cidade inválida - Menor que 2")]
+        public void CreateEndereco_ParametrosInvalidos_CidadeMenor()
+        {
+            Action action = () => new Endereco("15991-510", "M", "SP", "Rua Para", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("A cidade deve ter entre 2 e 100 caracteres.");
+        }
+
+        [Fact(DisplayName = "Cidade inválida - Maior que 100")]
+        public void CreateEndereco_ParametrosInvalidos_CidadeMaior()
+        {
+            var cidade = new string('a', 101);
+            Action action = () => new Endereco("15991-510", cidade, "SP", "Rua Para", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("A cidade deve ter entre 2 e 100 caracteres.");
+        }
+
+        [Fact(DisplayName = "Cidade inválida - Cidade vazia")]
+        public void CreateEndereco_ParametrosInvalidos_CidadeVazia()
+        {
+            Action action = () => new Endereco("15991-510", "", "SP", "Rua Para", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("A cidade deve ter entre 2 e 100 caracteres.");
+        }
+
+        [Fact(DisplayName = "Estado inválido - Diferente de 2")]
+        public void CreateEndereco_ParametrosInvalidos_EstadoDiferente()
+        {
+            Action action = () => new Endereco("15991-510", "Matão", "São", "Rua Para", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("O estado deve ter 2 caracteres.");
+        }
+
+        [Fact(DisplayName = "Estado inválido - Estado vazio")]
+        public void CreateEndereco_ParametrosInvalidos_EstadoVazio()
+        {
+            Action action = () => new Endereco("15991-510", "Matão", "", "Rua Para", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("O estado deve ter 2 caracteres.");
+        }
+
+        [Fact(DisplayName = "Logradouro inválido - Menor que 3")]
+        public void CreateEndereco_ParametrosInvalidos_LogradouroMenor()
+        {
+            Action action = () => new Endereco("15991-510", "Matão", "SP", "Av", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("O logradouro deve ter entre 3 e 150 caracteres.");
+        }
+
+        [Fact(DisplayName = "Logradouro inválido - Maior que 150")]
+        public void CreateEndereco_ParametrosInvalidos_LogradouroMaior()
+        {
+            var logradouro = new string('a', 151);
+            Action action = () => new Endereco("15991-510", "Matão", "SP", logradouro, 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("O logradouro deve ter entre 3 e 150 caracteres.");
+        }
+
+        [Fact(DisplayName = "Logradouro inválido - Logradouro vazio")]
+        public void CreateEndereco_ParametrosInvalidos_LogradouroVazio()
+        {
+            Action action = () => new Endereco("15991-510", "Matão", "SP", "", 12, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("O logradouro deve ter entre 3 e 150 caracteres.");
+        }
+
+        [Fact(DisplayName = "Número inválido - Número vazio")]
+        public void CreateEndereco_ParametrosInvalidos_NúmeroVazio()
+        {
+            Action action = () => new Endereco("15991-510", "Matão", "SP", "Rua Para", 0, new Guid());
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("O número deve ser maior que zero.");
+        }
+
+        [Fact(DisplayName = "ID inválido - ID de usuário inválido")]
+        public void CreateEndereco_ParametrosInvalidos_UsuarioVazio()
+        {
+            Action action = () => new Endereco("15991-510", "Matão", "SP", "Rua Para", 12, Guid.Empty);
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("ID de usuário inválido.");
+        }
+    }
+}
