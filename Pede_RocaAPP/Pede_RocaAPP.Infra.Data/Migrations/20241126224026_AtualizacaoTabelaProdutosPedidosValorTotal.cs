43a60abd874f561af62d1ed67983855ed617657b
HugoFrajacomo <hugofrajacomo@fatec.sp.gov.br>
2024-11-26 19:41:48 -0300

    #update medio

---
+++ Pede_RocaAPP/Pede_RocaAPP.Infra.Data/Migrations/20241126224026_AtualizacaoTabelaProdutosPedidosValorTotal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pede_RocaAPP.Infra.Data.Migrations
+{
+    public partial class AtualizacaoTabelaProdutosPedidosValorTotal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ValorTotal",
+                table: "ProdutosPedidos",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdUsuario",
+                table: "ComprasFinalizadas",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValorTotal",
+                table: "ProdutosPedidos");
+
+            migrationBuilder.DropColumn(
+                name: "IdUsuario",
+                table: "ComprasFinalizadas");
+        }
+    }
+}
