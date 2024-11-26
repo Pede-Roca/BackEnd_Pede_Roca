f680cf8cc9ade285b3b8e0575deb07ba99aa73f5
phs-santos <paulo18santos2000@gmail.com>
2024-11-26 19:38:24 -0300

    #update medio: corrigindo o produtospedidos unit test

--- Pede_RocaAPP/Pede_RocaAPP.Domain.Test/ProdutosPedidoUnitTest.cs
+++ Pede_RocaAPP/Pede_RocaAPP.Domain.Test/ProdutosPedidoUnitTest.cs
@@ -1,30 +1,33 @@
 ﻿using FluentAssertions;
 using Pede_RocaAPP.Domain.Entities;
 using Pede_RocaAPP.Domain.Validation;
+using System;
 
 namespace Pede_RocaAPP.Domain.Test
 {
     public class ProdutosPedidoUnitTest
     {
-        [Fact(DisplayName = "Pedido com quantidade de produtos valida")]
+        [Fact(DisplayName = "Pedido com quantidade de produtos válida")]
         public void CreateProdutosPedido_ParametrosValidos()
         {
-            Action action = () => new ProdutosPedido(10, Guid.NewGuid());
+            // Incluindo o valorTotal no construtor
+            Action action = () => new ProdutosPedido(10, Guid.NewGuid(), 100.00m);
             action.Should().NotThrow<DomainExceptionValidation>();
         }
 
         [Fact(DisplayName = "Id do produto inválido - Guid.Empty")]
         public void CreateProdutosPedido_ParametrosInvalidos_IdProdutoInvalido()
         {
- 
-            Action action = () => new ProdutosPedido(10, Guid.Empty);
+            // Incluindo o valorTotal no construtor
+            Action action = () => new ProdutosPedido(10, Guid.Empty, 100.00m);
             action.Should().Throw<DomainExceptionValidation>().WithMessage("Id do produto inválido, ele é obrigatório.");
         }
 
         [Fact(DisplayName = "Quantidade de produtos inválida - Menor que 1")]
         public void CreateProdutosPedido_ParametrosInvalidos_QuantidadeMenor()
         {
-            Action action = () => new ProdutosPedido(0, Guid.NewGuid());
+            // Incluindo o valorTotal no construtor
+            Action action = () => new ProdutosPedido(0, Guid.NewGuid(), 100.00m);
             action.Should().Throw<DomainExceptionValidation>().WithMessage("Quantidade de produto inválida, deve ser maior que 0.");
         }
     }
